b518a788988d3ae205d5a2668fa3e325e72736e7
aruberutoChan <albertoguerrerogarcia@gmail.com>
2018-08-09 02:37:36 +0200

    Initial commit

---
+++ src/Generators/ServiceGenerator.php
@@ -0,0 +1,195 @@
+<?php
+namespace Aruberuto\Workflow\Generators;
+use Prettus\Repository\Generators\Generator;
+use Prettus\Repository\Generators\Stub;
+use Prettus\Repository\Generators\ValidatorGenerator;
+use Prettus\Repository\Generators\RepositoryInterfaceGenerator;
+
+/**
+ * Class ControllerGenerator
+ * @package Aruberuto\Workflow\Generators
+ */
+class ServiceGenerator extends Generator
+{
+
+    /**
+     * Get stub name.
+     *
+     * @var string
+     */
+    protected $stub = 'service/service';
+
+    /**
+     * Get root namespace.
+     *
+     * @return string
+     */
+
+    public function getRootNamespace()
+    {
+        return str_replace('/', '\\', parent::getRootNamespace() . self::getConfigGeneratorClassPath($this->getPathConfigNode()));
+    }
+
+    /**
+     * Get generator path config node.
+     *
+     * @return string
+     */
+    public function getPathConfigNode()
+    {
+        return 'services';
+    }
+
+    /**
+     * Get stub template for generated file.
+     *
+     * @return string
+     */
+    public function getStub()
+    {
+        $path = config('repository.generator.stubsOverridePath', __DIR__);
+
+        if(!file_exists($path . '/Stubs/' . $this->stub . '.stub')){
+            $path = __DIR__;
+        }
+        return (new Stub($path . '/Stubs/' . $this->stub . '.stub', $this->getReplacements()))->render();
+    }
+
+    /**
+     * Get destination path for generated file.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        $path = $this->getBasePath() . '/' . self::getConfigGeneratorClassPath($this->getPathConfigNode(), true) . '/' . $this->getServiceName() . 'Service.php';
+
+        return $path;
+    }
+
+    /**
+     * Get base path of destination file.
+     *
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return config('repository.generator.basePath', app()->path());
+    }
+
+    /**
+     * Gets controller name based on model
+     *
+     * @return string
+     */
+    public function getServiceName()
+    {
+
+        return ucfirst(lcfirst(ucwords($this->getClass())));
+    }
+
+    /**
+     * Gets plural name based on model
+     *
+     * @return string
+     */
+    public function getPluralName()
+    {
+
+        return str_plural(lcfirst(ucwords($this->getClass())));
+    }
+
+    /**
+     * Get array replacements.
+     *
+     * @return array
+     */
+    public function getReplacements()
+    {
+
+        return array_merge(parent::getReplacements(), [
+            'service'    => $this->getServiceName(),
+            'plural'     => $this->getPluralName(),
+            'singular'   => $this->getSingularName(),
+            'validator'  => $this->getValidator(),
+            'repository' => $this->getRepository(),
+            'appname'    => $this->getAppNamespace(),
+        ]);
+    }
+
+    /**
+     * Gets singular name based on model
+     *
+     * @return string
+     */
+    public function getSingularName()
+    {
+        return str_singular(lcfirst(ucwords($this->getClass())));
+    }
+
+    /**
+     * Gets validator full class name
+     *
+     * @return string
+     */
+    public function getValidator()
+    {
+        $validatorGenerator = new ValidatorGenerator([
+            'name' => $this->name,
+        ]);
+
+        $validator = $validatorGenerator->getRootNamespace() . '\\' . $validatorGenerator->getName();
+
+        return 'use ' . str_replace([
+            "\\",
+            '/'
+        ], '\\', $validator) . 'Validator;';
+    }
+
+
+    /**
+     * Gets repository full class name
+     *
+     * @return string
+     */
+    public function getRepository()
+    {
+        $repositoryGenerator = new RepositoryInterfaceGenerator([
+            'name' => $this->name,
+        ]);
+
+        $repository = $repositoryGenerator->getRootNamespace() . '\\' . $repositoryGenerator->getName();
+
+        return 'use ' . str_replace([
+            "\\",
+            '/'
+        ], '\\', $repository) . 'Repository;';
+    }
+
+    /**
+     * Override parent Get class-specific output paths.
+     *
+     * @param $class
+     *
+     * @return string
+     */
+    public function getConfigGeneratorClassPath($class, $directoryPath = false)
+    {
+        if($class == 'services') {
+            $path = config('repository.generator.paths.services', 'Services');
+            if ($directoryPath) {
+                $path = str_replace('\\', '/', $path);
+            } else {
+                $path = str_replace('/', '\\', $path);
+            }
+
+            return $path;
+        } else {
+
+
+            return parent::getConfigGeneratorClassPath($class, $directoryPath = false);
+        }
+    }
+
+
+}
---
+++ src/WorkflowServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Aruberuto\Workflow;
+
+use Illuminate\Support\ServiceProvider;
+
+class WorkflowServiceProvider extends ServiceProvider
+{
+     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+
+    /**
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/resources/config/repository.php' => config_path('repository.php')
+        ]);
+
+        $this->mergeConfigFrom(   __DIR__ . '/resources/config/repository.php' , 'repository');
+
+        $this->loadTranslationsFrom(   __DIR__ . '/resources/lang' , 'workflow');
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands('Aruberuto\Workflow\Generators\Commands\ServiceCommand');
+        $this->commands('Aruberuto\Workflow\Generators\Commands\AruControllerCommand');
+        $this->commands('Aruberuto\Workflow\Generators\Commands\AruEntityCommand');
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
